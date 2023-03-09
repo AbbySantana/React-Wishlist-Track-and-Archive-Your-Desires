b484c266fb356aaa4aa5ada1df082e7097b83dba
AbbySantana <96422865+AbbySantana@users.noreply.github.com>
2023-03-09 13:14:58 +0000

    coloreado de los deseos segun el tiempo incompleto

--- dist/react-course-wishlist.78399e21.js
+++ dist/react-course-wishlist.78399e21.js
@@ -29985,21 +29985,53 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 var _classnames = _interopRequireDefault(require("classnames"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-// eslint-disable-next-line import/no-extraneous-dependencies
-
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 // eslint-disable-next-line react/function-component-definition
 var WishItem = function WishItem(_ref) {
   var done = _ref.done,
     text = _ref.text,
     id = _ref.id,
     onDoneChange = _ref.onDoneChange;
+  var _useState = (0, _react.useState)(0),
+    _useState2 = _slicedToArray(_useState, 2),
+    age = _useState2[0],
+    setAge = _useState2[1];
+  (0, _react.useEffect)(function () {
+    var ageinterval;
+    if (done) {
+      setAge(0);
+    } else {
+      ageinterval = setInterval(function () {
+        if (done) {
+          clearInterval(ageinterval);
+        } else {
+          // eslint-disable-next-line arrow-parens
+          setAge(function (a) {
+            return a + 1;
+          });
+        }
+      }, 1000);
+    }
+    return function () {
+      return clearInterval(ageinterval);
+    };
+  }, [done]);
   return /*#__PURE__*/_react.default.createElement("li", {
     className: (0, _classnames.default)('wish-list__item', {
-      'wish-list__item--done': done
+      'wish-list__item--done': done,
+      'wish-list__item--warning': age >= 5 && age < 10,
+      'wish-list__item--danger': age >= 10
     })
   }, /*#__PURE__*/_react.default.createElement("input", {
     id: id,
@@ -30208,7 +30240,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49248" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49615" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/App/WishItem/WishItem.jsx
+++ src/App/WishItem/WishItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -6,15 +6,44 @@ import classNames from 'classnames';
 // eslint-disable-next-line react/function-component-definition
 const WishItem = ({
   done, text, id, onDoneChange,
-}) => (
-  <li className={classNames('wish-list__item', {
-    'wish-list__item--done': done,
-  })}
-  >
-    <input id={id} checked={done} onChange={(e) => onDoneChange(e.target.checked)} type="checkbox" />
-    <label htmlFor={id}>{text}</label>
-  </li>
-);
+}) => {
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    let ageinterval;
+    if (done) {
+      setAge(0);
+    } else {
+      ageinterval = setInterval(() => {
+        if (done) {
+          clearInterval(ageinterval);
+        } else {
+          // eslint-disable-next-line arrow-parens
+          setAge(a => a + 1);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(ageinterval);
+  }, [done]);
+
+  return (
+    <li
+      className={classNames('wish-list__item', {
+        'wish-list__item--done': done,
+        'wish-list__item--warning': age >= 5 && age < 10,
+        'wish-list__item--danger': age >= 10,
+      })}
+    >
+      <input
+        id={id}
+        checked={done}
+        onChange={(e) => onDoneChange(e.target.checked)}
+        type="checkbox"
+      />
+      <label htmlFor={id}>{text}</label>
+    </li>
+  );
+};
 
 WishItem.propTypes = {
   done: PropTypes.bool,
