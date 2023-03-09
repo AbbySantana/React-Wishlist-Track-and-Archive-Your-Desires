c94bc82016ad911eab01163e64dc162c71e9946c
AbbySantana <96422865+AbbySantana@users.noreply.github.com>
2023-03-09 11:11:51 +0000

    separar la Wish List en  diferentes componentes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import classNames from 'classnames';
+
 import './App.css';
+import WishInput from './WishInput';
+import WishList from './WishList';
 
 const wishes = [
   { done: false, text: 'Travel to the moon' },
@@ -11,31 +13,16 @@ const wishes = [
 
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My wishlist</h1>
-      <fieldset className="wish-input">
-        <legend className="wish-input__label">New wish</legend>
-        <input className="wish-input_field" placeholder="Enter your wish here" />
-      </fieldset>
-      <ul className="wish-list">
-        {wishes.map(({ text, done }, i) => (
-          <li
-            key={text}
-            className={classNames(
-              'wish-list__item',
-              { 'wish-list__item--done': done },
-            )}
-          >
-            <input id={`wish${i}`} type="checkbox" checked={done} />
-            <label htmlFor={`wish${i}`}>{text}</label>
-          </li>
-        ))}
-      </ul>
-      <button className="wish-clear" type="button">Archive done</button>
-    </div>
-  );
-}
+// eslint-disable-next-line react/function-component-definition
+const App = () => (
+  <div className="app">
+    <h1>My wishlist</h1>
+    <WishInput />
+    <WishList wishes={wishes} />
+    <button className="wish-clear" type="button">
+      Archive done
+    </button>
+  </div>
+);
 
 export default App;
---
+++ src/App/WishInput/WishInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// eslint-disable-next-line react/function-component-definition
+const WishInput = () => (
+  <fieldset className="wish-input">
+    <legend className="wish-input__label">New wish</legend>
+    <input className="wish-input_field" placeholder="Enter your wish here" />
+  </fieldset>
+);
+
+export default WishInput;
